ed0eda6c4c63fddf22db08f24529666afc014ecb
Arthur <arthuralmeida040208@gmail.com>
2026-09-16 13:10:25 -0300

    security: implementar variaveis de ambiente para senhas no DataSeeder (12-Factor App)

--- src/main/java/com/produtoapi/config/DataSeeder.java
+++ src/main/java/com/produtoapi/config/DataSeeder.java
@@ -1,19 +1,30 @@
 package com.produtoapi.config;
 
 import com.produtoapi.model.Produto;
+import com.produtoapi.model.Usuario;
 import com.produtoapi.repository.ProdutoRepository;
+import com.produtoapi.repository.UsuarioRepository;
 import com.produtoapi.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
+import org.springframework.beans.factory.annotation.Value;
 @Component
 public class DataSeeder implements CommandLineRunner {
 
     // O método 'run' é a primeira coisa que o Spring executa ao ligar!
     @Autowired
     ProdutoRepository produtoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+
+    @Value("${ADMIN_PASSWORD:123456}")
+    private String adminPassword;
+
+    @Value("${OPERADOR_PASSWORD:123456}")
+    private String operadorPassword;
 
 
     @Override
@@ -46,6 +57,35 @@ public class DataSeeder implements CommandLineRunner {
             System.out.println("Produtos cadastrados com sucesso pelo Seeder!");
         }
 
-        // Aqui dentro vai entrar a nossa lógica!
+        // Criação automática do usuário DONO
+        if (usuarioRepository.findByEmail("admin@drop21.com").isEmpty()) {
+            Usuario admin = new Usuario();
+            admin.setNome("Chefe Drop 21");
+            admin.setEmail("admin@drop21.com");
+
+            org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder encoder = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+
+            // OLHA A MUDANÇA AQUI (sem aspas):
+            admin.setSenha(encoder.encode(adminPassword));
+
+            admin.setRole("ROLE_DONO");
+            usuarioRepository.save(admin);
+        }
+
+        // Criação automática do usuário OPERADOR (Caixa)
+        if (usuarioRepository.findByEmail("operador@drop21.com").isEmpty()) {
+            Usuario operador = new Usuario();
+            operador.setNome("Caixa Drop 21");
+            operador.setEmail("operador@drop21.com");
+
+            org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder encoder = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+
+            // OLHA A MUDANÇA AQUI (sem aspas):
+            operador.setSenha(encoder.encode(operadorPassword));
+
+            operador.setRole("ROLE_OPERADOR");
+            usuarioRepository.save(operador);
+        }
+
     }
 }
